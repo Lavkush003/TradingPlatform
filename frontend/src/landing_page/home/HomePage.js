9efd7a9327f8197ad30ec11498743f805f343f9b
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-03-01 17:49:45 +0530

    all errros fixed

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -6,7 +6,7 @@ import Pricing from "./Pricing";
 import Education from "./Education";
 import OpenAccount from "../OpenAccount";
 import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 function HomePage() {
   return (
     <>
@@ -17,7 +17,7 @@ function HomePage() {
       <Pricing />
       <Education />
       <OpenAccount />
-     <Footer />
+    
     </>
   );
 }
